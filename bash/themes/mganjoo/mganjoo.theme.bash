15fc88eedcc114f47ba272caced5874288ff21b4
Milind Ganjoo <mganjoo@stanford.edu>
2012-12-05 06:20:39 -0800

    Add new theme

---
+++ bash/themes/mganjoo/mganjoo.theme.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+export GIT_THEME_PROMPT_PREFIX="\("
+export GIT_THEME_PROMPT_SUFFIX="\)"
+
+function prompt_command() {
+PS1="${yellow}\h${reset_color}:${cyan}\W${reset_color} ${green}$(scm_prompt_info) ${purple}>>${reset_color} ";
+}
+
+PROMPT_COMMAND=prompt_command;
