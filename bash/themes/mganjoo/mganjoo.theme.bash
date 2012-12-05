c5adbab294a254d13b9f299cbffa64b5903a7c81
Milind Ganjoo <mganjoo@stanford.edu>
2012-12-05 06:34:49 -0800

    Fix extra space bug in mganjoo prompt

--- bash/themes/mganjoo/mganjoo.theme.bash
+++ bash/themes/mganjoo/mganjoo.theme.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export GIT_THEME_PROMPT_PREFIX=" ("
-export GIT_THEME_PROMPT_SUFFIX=") "
+export GIT_THEME_PROMPT_SUFFIX=")"
 
 function prompt_command() {
 PS1="${yellow}\h${reset_color}:${cyan}\W${reset_color}${green}$(scm_prompt_info)${purple} >>${reset_color} ";
